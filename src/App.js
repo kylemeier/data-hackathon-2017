3543a0362f343fe29843bee404baf4e75ce4f4fe
Kyle Meier <kyledmeier@gmail.com>
2017-09-01 10:20:15 -0400

    creating repo

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img
+            src="http://execonlinenew.wpengine.com/wp-content/uploads/2017/04/EXO_logo_tagline_white_180.png"
+            className="App-logo"
+          />
+          {/* <h2>Data Visualization</h2> */}
+        </div>
+        <p className="App-intro">Graph!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
